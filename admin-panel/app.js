ec0174e348e8e14ff4dd79ba6d8afeabc3352b94
sanjaymajhi <1sanjaymajhi@gmail.com>
2020-04-07 02:52:07 +0530

    added changes neccessary for deploying

--- admin-panel/app.js
+++ admin-panel/app.js
@@ -20,6 +20,18 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api", userRoutes);
 
+var compression = require("compression");
+app.use(compression());
+
+var helmet = require("helmet");
+app.use(helmet());
+
+app.get("/*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "./client/admin-panel-client/build", "index.html")
+  );
+});
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
