fdb055157f1c4c5fa274593f38496266f13f8359
sanjaymajhi <1sanjaymajhi@gmail.com>
2020-04-07 12:17:51 +0530

    some changes

---
+++ admin-panel/build/precache-manifest.f5f99356639e168cba4209b31ca0249c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da60d6d5f0a221915a053644826bb4b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49989582f7a9b10c9f42",
+    "url": "/static/css/main.04bea088.chunk.css"
+  },
+  {
+    "revision": "59119b47cd4bb3e2b197",
+    "url": "/static/js/2.cc07c2a0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cc07c2a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49989582f7a9b10c9f42",
+    "url": "/static/js/main.3303cf82.chunk.js"
+  },
+  {
+    "revision": "a4ecc74fc0b321b7a610",
+    "url": "/static/js/runtime-main.8e9c86d8.js"
+  }
+]);
